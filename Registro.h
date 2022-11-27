f25e9f6f1e120729cae2889229ab0a4b94e57cf6
Bowist27 <111481792+Bowist27@users.noreply.github.com>
2022-11-27 15:16:16 -0600

    Update Registro.h

--- Registro.h
+++ Registro.h
@@ -1,4 +1,4 @@
-#include <string>
+#include "string.h"
 using namespace std;
 
 //Implementacion de la clase Dia anio
@@ -6,15 +6,20 @@ using namespace std;
 class Registro{
     //Atributos
     private:
-        int idusuario,contrasena;
+        string idusuario,contrasena;
 
     //Metodos
     public:          //CONSTRUCTOR
-        Registro(int _idusuario, int _contrasena){
-            idusuario = _idusuario;
-            contrasena = _contrasena;
-        }
-
-        int ingresardatos(Registro& d);   //Es objeto dinamico
-        bool verificarlogin();
+        Registro()
+        {
+            idusuario = "\0";
+            contrasena = "\0";
+        };
+        bool Ingresar();
 };
+
+//Primera Funcion = Darle un valor a variable usuario
+//Parametro = Se toma un valor de tipo int
+//Retorno = No regresa nada
+
+    
