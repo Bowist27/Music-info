961f297651a3076d5e8fc5de34c8934a5492303d
Bowist27 <111481792+Bowist27@users.noreply.github.com>
2022-11-30 15:01:00 -0600

    Update Album.h

--- Album.h
+++ Album.h
@@ -1,12 +1,35 @@
-#include <string>
-using namespace std;
+#ifndef ALBUM
+#define ALBUM
 
-class Album
-{
+#include "GeneroMusical.h"
+#include "DatoCurioso.cpp"
+
+#include <iostream>
+#include <string>
+
+//Herencia
+class Album:public GeneroMusical{
     private:
         string fecha_lanzamiento, numero_canciones;
 
+        DatoCurioso datoCurioso;
+
     public:
         Album();
-        void ImprimirDatos(string);
+        // (genero_musical,instrumento recurrente, fecha_lanzamiento,numero_canciones, datoCurioso)
+        Album(string,string, string,string, string);
+
+        // Setters
+        void setFechaLanzamiento(string);
+        void setNumeroCanciones(string);
+
+        // Viene de datoCurioso
+        void agregaDatoCurioso(DatoCurioso,string);
+
+        string getFechaLanzamiento();
+        string getNumeroCanciones();
+
+        // Super string que se va a usar
+        string getAlbumInfo ();
 };
+#endif
